9042e5260eea0b2638f3c30abb46bf24c52e2987
tannerbitz <tannerbitz@gmail.com>
2017-01-09 16:27:11 -0700

    Update printDialog.cpp

--- printDialog.cpp
+++ printDialog.cpp
@@ -5,5 +5,37 @@
 int main()
 {
     string pronoun;
+    std::cout << "What are you?" << std::endl;
+    std::cout << "- Friend \n -Buddy \n -Guy" << std::endl;
+    std::cin >> pronoun >> std::endl;
+    
+    while (pronoun != ('Friend' || 'Buddy' || 'Guy'))
+    {
+        std::cout << "You can only type Friend, Buddy, or Guy" << std::endl;
+        std::cout << "So, what are you?" << std::endl;
+        std::cout << "- Friend \n -Buddy \n -Guy" << std::endl;
+        std::cin >> pronoun >> std::endl;
+    }
+    
+    for (int i = 0 , i < 6 , i++)
+    {
+        if (pronoun = 'Friend')
+        {
+            std::cout << "I'm not your friend, buddy" << std::endl;
+            pronoun = 'Buddy'
+        }
+        elseif (pronoun = 'Buddy')
+        {
+            std::cout << "I'm not your buddy, guy" << std::endl;
+            pronoun = 'Guy'
+        }
+        else (pronoun = 'Guy')
+        {
+            std::cout << "I'm not your guy, friend" << std::endl;
+            pronoun = 'Friend'            
+        }
+        
+    }
+    
     
 }
