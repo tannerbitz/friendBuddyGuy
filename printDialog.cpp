79fb3890c78f49dcf4884b69f139b27db42f43e6
tannerbitz <tannerbitz@gmail.com>
2017-01-09 16:58:49 -0700

    Update printDialog.cpp

--- printDialog.cpp
+++ printDialog.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 #include <string.h>
-
+#include <typeinfo>
 
 int main()
 {
-    string pronoun;
+    std::string pronoun;
     std::cout << "What are you?" << std::endl;
-    std::cout << "- Friend \n -Buddy \n -Guy" << std::endl;
-    std::cin >> pronoun >> std::endl;
-    
-    while (pronoun != ('Friend' || 'Buddy' || 'Guy'))
+    std::cout << "- Friend \n - Buddy \n - Guy" << std::endl;
+    std::cin >> pronoun;
+
+
+    while (pronoun.compare("Friend") != 0 && pronoun.compare("Buddy") != 0 && pronoun.compare("Guy") != 0)
     {
         std::cout << "You can only type Friend, Buddy, or Guy" << std::endl;
         std::cout << "So, what are you?" << std::endl;
         std::cout << "- Friend \n -Buddy \n -Guy" << std::endl;
-        std::cin >> pronoun >> std::endl;
+        std::cin >> pronoun;
     }
-    
-    for (int i = 0 , i < 6 , i++)
+
+    for (int i = 0; i < 6; i++)
     {
-        if (pronoun = 'Friend')
+        if (pronoun.compare("Friend") == 0)
         {
             std::cout << "I'm not your friend, buddy" << std::endl;
-            pronoun = 'Buddy'
+            pronoun = "Buddy";
         }
-        elseif (pronoun = 'Buddy')
+        else if (pronoun.compare("Buddy") == 0)
         {
             std::cout << "I'm not your buddy, guy" << std::endl;
-            pronoun = 'Guy'
+            pronoun = "Guy";
         }
-        else (pronoun = 'Guy')
+        else if(pronoun.compare("Guy") == 0)
         {
             std::cout << "I'm not your guy, friend" << std::endl;
-            pronoun = 'Friend'            
+            pronoun = "Friend";
         }
-        
+
     }
-    
-    
+
+    return 0;
 }
+
